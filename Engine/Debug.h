7dbb9f9b59b3fef5150566da177ba3f6ad59eb9b
RhinoWare <84024458+RhinoEngineFounder@users.noreply.github.com>
2022-08-17 21:16:23 +0200

    Delete Debug.h

--- Engine/Debug.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-#define DEBUG(m, x) std::cout << m << x << std::endl;
-#define MESSAGE(m) std::cout << m << std::endl;
-
-#define FATAL_ERROR(m) \
-        std::cerr << m << std::endl;
-        //std::terminate();
